7534217381e444b17b9f2bc255d57bd9d30e98ec
Paralaxe <primataxdeus@gmail.com>
2017-05-11 22:20:28 -0300

    the source

---
+++ Nanodegree/P0/chopstick.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+path = r'~/Downloads/chopstick-effectiveness.csv'
+
+dataFrame = pd.read_csv(path)
+dataFrame
+
---
+++ chopstick.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+path = 'chopstick-effectiveness.csv'
+path = r'~/Documents/Data Science/Nanodegree/P0/chopstick-effectiveness.csv'
+dataFrame = pd.read_csv(path)
+dataFrame
+
